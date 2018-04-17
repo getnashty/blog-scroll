c4ba2c031f61e53705e082352ae1bb234184afe4
Elliot Nash <elliot@lolatravel.com>
2018-04-17 13:26:12 -0400

    Step 1: setupbasic scrollview

--- App.js
+++ App.js
@@ -1,23 +1,68 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, ScrollView, StatusBar, View } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+
+    renderRow(num = 1) {
+        return [...Array(num)].map((val, index) => {
+            return (
+                <View style={styles.row} key={index}>
+                    <Text style={styles.text}>
+                        My scrollable text
+                    </Text>
+                </View>
+            );
+        });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <StatusBar hidden />
+                <View style={styles.header}>
+                    <Text style={styles.textHeader}>
+                        My header
+                    </Text>
+                </View>
+                <ScrollView style={styles.scrollView}>
+                    {this.renderRow(20)}
+                </ScrollView>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#000',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    height: 100
+  },
+  scrollView: {
+    flex: 1
+  },
+  row: {
+    paddingVertical: 30,
+    marginTop: 10,
+    marginHorizontal: 10,
+    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+    borderRadius: 2
+  },
+  text: {
+    textAlign: 'center',
+    fontFamily: 'AvenirNext-Medium',
+    fontSize: 14,
+    color: '#FFF'
+  },
+  textHeader: {
+    textAlign: 'center',
+    fontFamily: 'AvenirNext-DemiBold',
+    fontSize: 16,
+    color: '#000'
+  }
 });
